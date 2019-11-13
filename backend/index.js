f7ac65934f57bc8dba0b94a4b1fa6b3f1eddb79a
mariam-rivera <mariamhdez@gmail.com>
2019-11-13 09:39:08 -0600

    Server initialized.

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const morgan = require('morgan');
+const express = require('express');
+const app = express();
+const { mongoose } = require('./database');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+
+// middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// routes
+app.use('/api/users', require('./routes/user.routes'));
+
+// starting server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
---
+++ backend/routes/user.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const user = require('../controllers/user.controllers');
+
+router.get('/', user.getUsers);
+router.post('/', user.createUser);
+router.get('/:id', user.getUser);
+
+module.exports = router;
