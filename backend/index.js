41c5fb58254ffe6abb7ccf7fc6005ecaf7572954
mariam-rivera <mariamhdez@gmail.com>
2019-11-14 11:41:44 -0600

    Dotenv and jwt added.

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,37 @@ const express = require('express');
 const app = express();
 const { mongoose } = require('./database');
 
+const jwt = require('jsonwebtoken');
+const fs = require('fs')
+
+// dotenv
+app.get('/', (req, res) => {
+    res.send(process.env.db_password);
+});
+
+// jwt
+app.get('/secret', (req, res) => {
+    res.json({
+        "message": "Super secret message"
+    });
+});
+
+app.get('/readme', (req, res) => {
+    res.json({
+        "message": "Hello world!"
+    });
+});
+
+app.get('/jwt', (req, res) => {
+    let privateKey = fs.readFileSync('./priv.pem', 'utf8');
+    let token = jwt.sign({
+        "body": "stuff"
+    }, privateKey, {
+        algorithm: 'HS256'
+    });
+    res.send(token);
+});
+
 // settings
 app.set('port', process.env.PORT || 3000);
 
