7e7490a3d870dcb00a965d688f307cd18d56ce0e
Hegar Garcia <hegargarcia@gmail.com>
2019-12-03 20:20:33 -0600

    Fixing spacing rule

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,27 +1,27 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema(
-    {
-        fullName: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: false
-        },
-        codeMorse: {
-            type: String,
-            required: false
-        }
+const UserSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true
     },
-    {
-        versionKey: false
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: false
+    },
+    codeMorse: {
+      type: String,
+      required: false
     }
+  },
+  {
+    versionKey: false
+  }
 );
 
-module.exports = model("User", userSchema);
+module.exports = model("User", UserSchema);
